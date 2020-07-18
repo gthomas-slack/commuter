2c08eea5ff7db112f2bdc8ccb1c810a35f904b64
Glenn Thomas <gthomas@slack-corp.com>
2020-07-18 12:44:04 -0700

    hack

--- src/index.js
+++ src/index.js
@@ -8,9 +8,4 @@ createServer()
     const port = server.address().port;
     console.log(log);
     log.info("Commuter server listening on port " + port);
-  })
-  .catch((e: Error) => {
-    console.error(e);
-    console.error(e.stack);
-    process.exit(-10);
   });
